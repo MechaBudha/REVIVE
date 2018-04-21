3677620a89af9e7bb4a2eb85b59a437e18dba923
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-04-21 18:15:10 -0300

    Sonido del disparo, prototipo recarga

--- JuegoN1RuizTorres/Assets/Scripts/Weapon/Weapon.cs
+++ JuegoN1RuizTorres/Assets/Scripts/Weapon/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
 
     private Animator anim;
+    private AudioSource _AudioSource;
 
     public float range = 100f;                                              //Distancia maxima del arma
     public int bulletsPerMag = 30;                                          //Balas en cada cargador
@@ -13,22 +14,29 @@ public class Weapon : MonoBehaviour {
     public int currentBullets;                                              //Las balas que tengo actualmente en el cargador
 
     public Transform shootPoint;                                            //El punto de donde salen las balas
+    public ParticleSystem muzzleFlash;
+    public AudioClip shootSound;
 
     public float fireRate = 0.1f;                                           //El delay entre cada disparo
 
     float fireTimer;                                                        //Contador de tiempo para el delay
 
-	void Start () {
-
+	void Start ()
+    {
         anim = GetComponent<Animator>();
+        _AudioSource = GetComponent<AudioSource>();
         currentBullets = bulletsPerMag;
 	}
 	
-	void Update () {
+	void Update ()
+    {
 		
         if (Input.GetButton("Fire1"))
         {
-            Fire();                                                         //Ejecuta la funcion fire si es que presionamos/mantenemos el click izquierdo del mouse
+            if (currentBullets > 0)
+                Fire();                                                     //Ejecuta la funcion fire si es que presionamos/mantenemos el click izquierdo del mouse
+            else
+                Reload();
         }
 
         if (fireTimer < fireRate)
@@ -39,11 +47,12 @@ public class Weapon : MonoBehaviour {
     {
         AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
 
-        if (info.IsName("Fire")) anim.SetBool("Fire", false);               //Si estamos en el Fire State, reseteamos la variable
+        //if (info.IsName("Fire")) anim.SetBool("Fire", false);             //Si estamos en el Fire State, reseteamos la variable
     }
 
-    private void Fire() {                                                   //Este disparo va a estar hecho con raycast (castea un rayacst y no crea una bala cada vez que dispara)
-        if (fireTimer < fireRate) return;                                   //Esto seria como el verificador que pregunta si podemos disparar, si el timer terminó
+    private void Fire()
+    {                                                   //Este disparo va a estar hecho con raycast (castea un rayacst y no crea una bala cada vez que dispara)
+        if (fireTimer < fireRate || currentBullets <= 0) return;            //Esto seria como el verificador que pregunta si podemos disparar, si el timer terminó, y si las balas llegan a 0 que pare.
 
         RaycastHit hit;                                                     //Aca vamos a guardar los resultados de donde golpea el raycast
 
@@ -54,7 +63,27 @@ public class Weapon : MonoBehaviour {
         }
 
         anim.CrossFadeInFixedTime("Fire", 0.01f);                           //Ejecuta la animacion de disparo
+        muzzleFlash.Play();                                                 //Ejecuta el fogonazo del arma
+        PlayShootSound();                                                   //Ejecuta el sonido del disparo
+
         currentBullets--;
         fireTimer = 0.0f;                                                   //Resetea el fire timer
+    }
+
+    private void Reload()
+    {
+        if (bulletsLeft <= 0) return;                                       //Si no te quedan mas balas no podes recargar
+
+        int bulletsToLoad = bulletsPerMag - currentBullets;                 //Esto se fija cuantas balas hay que cargar en el cargador
+        int bulletsToDeduct = (bulletsLeft >= bulletsToLoad) ? bulletsToLoad : bulletsLeft;
+
+        bulletsLeft -= bulletsToDeduct;
+        currentBullets += bulletsToDeduct;
+    }
+
+    private void PlayShootSound()
+    {
+        _AudioSource.clip = shootSound;
+        _AudioSource.Play();
     }
 }
