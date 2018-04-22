53d1b51767c48318615357956df17031ea5efba0
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-04-22 14:41:57 -0300

    Sonido del disparo arreglado, recarga funcional

--- JuegoN1RuizTorres/Assets/Scripts/Weapon/Weapon.cs
+++ JuegoN1RuizTorres/Assets/Scripts/Weapon/Weapon.cs
@@ -21,42 +21,52 @@ public class Weapon : MonoBehaviour {
 
     float fireTimer;                                                        //Contador de tiempo para el delay
 
-	void Start ()
+    private bool isReloading;                                               //Ve si estamos en la animacion de Reload
+
+    void Start()
     {
         anim = GetComponent<Animator>();
         _AudioSource = GetComponent<AudioSource>();
         currentBullets = bulletsPerMag;
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
-		
+
         if (Input.GetButton("Fire1"))
         {
             if (currentBullets > 0)
                 Fire();                                                     //Ejecuta la funcion fire si es que presionamos/mantenemos el click izquierdo del mouse
-            else
-                Reload();
+            else if(bulletsLeft > 0)
+                DoReload();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (currentBullets < bulletsPerMag && bulletsLeft > 0)
+                DoReload();
         }
 
         if (fireTimer < fireRate)
             fireTimer += Time.deltaTime;                                    //Add into time counter
-	}
+    }
 
     private void FixedUpdate()
     {
         AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
 
+        isReloading = info.IsName("Reload");                                //Si estamos en el Reload state, isReloading va a ser true por defercto
+
         //if (info.IsName("Fire")) anim.SetBool("Fire", false);             //Si estamos en el Fire State, reseteamos la variable
     }
 
     private void Fire()
-    {                                                   //Este disparo va a estar hecho con raycast (castea un rayacst y no crea una bala cada vez que dispara)
-        if (fireTimer < fireRate || currentBullets <= 0) return;            //Esto seria como el verificador que pregunta si podemos disparar, si el timer terminó, y si las balas llegan a 0 que pare.
+    {                                                                       //Este disparo va a estar hecho con raycast (castea un rayacst y no crea una bala cada vez que dispara)
+        if (fireTimer < fireRate || currentBullets <= 0 || isReloading) return; //Esto seria como el verificador que pregunta si podemos disparar, si el timer terminó, y si las balas llegan a 0 que pare.
 
         RaycastHit hit;                                                     //Aca vamos a guardar los resultados de donde golpea el raycast
 
-        if(Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range)) //Este es el disparo en sí
+        if (Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range)) //Este es el disparo en sí
         {
             Debug.Log(hit.transform.name + " found!");                      //Muestra el nombre del objeto al que golpea el raycast
 
@@ -70,7 +80,7 @@ public class Weapon : MonoBehaviour {
         fireTimer = 0.0f;                                                   //Resetea el fire timer
     }
 
-    private void Reload()
+    public void Reload()
     {
         if (bulletsLeft <= 0) return;                                       //Si no te quedan mas balas no podes recargar
 
@@ -81,9 +91,19 @@ public class Weapon : MonoBehaviour {
         currentBullets += bulletsToDeduct;
     }
 
+    private void DoReload()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (isReloading) return;
+
+        anim.CrossFadeInFixedTime("Reload", 0.01f);
+    }
+
     private void PlayShootSound()
     {
-        _AudioSource.clip = shootSound;
-        _AudioSource.Play();
+        _AudioSource.PlayOneShot(shootSound);                               //Con este podemos reproducir muchos audios
+        //_AudioSource.clip = shootSound;                                   //Con estos dos el audio se cortaba
+        //_AudioSource.Play();
     }
 }
