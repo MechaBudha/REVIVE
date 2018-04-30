1f48216e327607c26b9ab9edd2b468a6aa1f8605
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-04-30 15:58:56 -0300

    Nivel avanzado, linterna funcional, musica de fondo

---
+++ JuegoN1RuizTorres/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    [SerializeField] private Light flashLight;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip flashlightSwitch;
+
+    private bool isActive;
+
+    private void Start()
+    {
+        isActive = false;
+    }
+
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!isActive)
+            {
+                flashLight.enabled = true;
+                isActive = true;
+                audioSource.PlayOneShot(flashlightSwitch);
+            }
+            else if (isActive)
+            {
+                flashLight.enabled = false;
+                isActive = false;
+                audioSource.PlayOneShot(flashlightSwitch);
+            }
+        }
+	}
+}
