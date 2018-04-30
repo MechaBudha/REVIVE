1adb90a6d946ecd2a85708bc5c6a5cb5819ab5b9
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-04-29 22:38:34 -0300

    Boton que abre puerta, animacion de puerta

---
+++ JuegoN1RuizTorres/Assets/Scripts/Gizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour {
+
+	[SerializeField] private float gizmoSize = .75f;
+	[SerializeField] private Color gizmoColor = Color.yellow;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
