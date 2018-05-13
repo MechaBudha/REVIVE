3adae451815efc36f18667f85921e8619368c75b
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-05-13 01:03:26 -0300

    Se agrego la pantalla de creditos

--- JuegoN1RuizTorres/Assets/Scripts/Player/PlayerMove.cs
+++ JuegoN1RuizTorres/Assets/Scripts/Player/PlayerMove.cs
@@ -6,10 +6,13 @@ public class PlayerMove : MonoBehaviour
 {
     CharacterController charControl;
     public float walkSpeed;
+    //public GameObject weapon;
+    //private Animator anim;
 
     void Awake()
     {
         charControl = GetComponent<CharacterController>();
+        //anim = weapon.GetComponent<Animator>();
     }
 
     void Update()
@@ -22,11 +25,29 @@ public class PlayerMove : MonoBehaviour
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
 
+        if (horiz != 0)
+        {
+            PlayAnimation();
+        }
+        if (vert != 0)
+        {
+            PlayAnimation();
+        }
+
         Vector3 moveDirSide = transform.right * horiz * walkSpeed;
         Vector3 moveDirForward = transform.forward * vert * walkSpeed;
 
         charControl.SimpleMove(moveDirSide);
         charControl.SimpleMove(moveDirForward);
 
+        
+    }
+
+    void PlayAnimation()
+    {
+        //anim.CrossFadeInFixedTime("Walk", 1f);
+        //anim.Play("Walk");
+        //anim.Play("Walk");
+        
     }
 }
