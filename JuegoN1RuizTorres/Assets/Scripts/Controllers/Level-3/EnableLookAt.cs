7e48e04e161aa5963e6351dd2c7ef039889eb0d2
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-07-09 20:25:08 -0300

    2.6.0-alpha
    Add Level 3 to the project
    
    -Add "Level3" scene.
    
    -Add "Background_music5" file
    
    -Add "Level3" scripts

---
+++ JuegoN1RuizTorres/Assets/Scripts/Controllers/Level-3/EnableLookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLookAt : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<LookAtController>().enabled = true;
+
+    }
+}
---
+++ JuegoN1RuizTorres/Assets/Scripts/Controllers/Level-3/LookAtController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtController : MonoBehaviour {
+
+
+    //[SerializeField] Transform target;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float rotationDamping;
+
+    void Update () {
+        //transform.LookAt(target);
+
+        /*
+        Vector3 dir = targetPoint - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, lookRotation, rotationDamping * Time.deltaTime);
+        transform.rotation = newRotation;
+        */
+
+        Vector3 angles = transform.rotation.eulerAngles;
+        Quaternion from = Quaternion.Euler(angles.x, angles.y, 0);
+
+        Vector3 dir = targetPoint.position - transform.position;
+        Quaternion to = Quaternion.LookRotation(dir);
+
+        transform.rotation = Quaternion.RotateTowards(from, to, rotationDamping * Time.deltaTime);
+
+        
+    }
+}
