04b527d9abf9c9a977503d2c7febd59f1bfa6fe0
Matias <ruiztorresmatias@gmail.com>
2018-10-30 15:54:09 -0300

    2.9.10-alpha
    - SceneLoader script done
    - LoadingL1 done

--- JuegoN1RuizTorres/Assets/Scripts/ScenesScripts/SceneLoader.cs
+++ JuegoN1RuizTorres/Assets/Scripts/ScenesScripts/SceneLoader.cs
@@ -8,52 +8,27 @@ public class SceneLoader : MonoBehaviour {
     [SerializeField] private int scene;
     [SerializeField] private Text loadingText;
 
-    void Update()
-    {
-        // If the player has pressed the space bar and a new scene is not loading yet...
-        if (Input.GetKeyUp(KeyCode.Space) && loadScene == false)
-        {
+    void Update() {
 
-            // ...set the loadScene boolean to true to prevent loading a new scene more than once...
+        if (loadScene == false) {
             loadScene = true;
-
-            // ...change the instruction text to read "Loading..."
             loadingText.text = "Loading...";
-
-            // ...and start a coroutine that will load the desired scene.
             StartCoroutine(LoadNewScene());
-
         }
 
-        // If the new scene has started loading...
-        if (loadScene == true)
-        {
-
-            // ...then pulse the transparency of the loading text to let the player know that the computer is still working.
+        if (loadScene == true) 
             loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
-
-        }
 
     }
 
+    IEnumerator LoadNewScene() {
 
-    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
-    IEnumerator LoadNewScene()
-    {
+        yield return new WaitForSeconds(3);
 
-        // This line waits for 3 seconds before executing the next line in the coroutine.
-        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
-        //yield return new WaitForSeconds(3);
-
-        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
         AsyncOperation async = Application.LoadLevelAsync(scene);
 
-        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
-        while (!async.isDone)
-        {
+        while (!async.isDone) 
             yield return null;
-        }
-
     }
 
 }
