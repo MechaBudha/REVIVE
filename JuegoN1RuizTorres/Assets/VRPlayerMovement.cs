f05ccf2568a3cd21d70eb854f1b703da6f0ad693
Unknown <tmugetti@gmail.com>
2018-11-27 12:00:54 -0300

    2.9.18-alpha
    Vew VR prefab on fevelopment

---
+++ JuegoN1RuizTorres/Assets/VRPlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayerMovement : MonoBehaviour {
+	[SerializeField] private CharacterController charControl;
+	[SerializeField] private float walkSpeed;
+	private InputManager inputMG;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
